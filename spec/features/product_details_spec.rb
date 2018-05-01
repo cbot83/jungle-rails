d4a8635e406d35da15d46ce9c1592b3d85194df9
Craig Rice <craigricetong@gmail.com>
2018-05-01 16:56:53 +0000

    prepared a spec for product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details", type: :feature, js: true do
+  
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They click to view a product" do
+    # ACT
+    visit root_path
+    # need a new act to click on product
+    visit '/products/2'
+
+    # DEBUG
+    save_screenshot 'products.png'
+    puts page.html
+
+    # VERIFY
+    # need a new expect for product visit
+    # expect(page).to have_css 'article.product', count: 10
+  end
+
+end
